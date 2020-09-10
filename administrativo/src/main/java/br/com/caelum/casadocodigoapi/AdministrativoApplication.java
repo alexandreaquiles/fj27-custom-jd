42f41f8e5a2182102bc77acf9cf70863cc79e427
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-09-10 17:45:15 -0300

    Tarefa 4 - extraindo serviço administrativo e bye bye monólito modular

--- administrativo/src/main/java/br/com/caelum/casadocodigoapi/AdministrativoApplication.java
+++ administrativo/src/main/java/br/com/caelum/casadocodigoapi/AdministrativoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasadocodigoApiApplication {
+public class AdministrativoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CasadocodigoApiApplication.class, args);
+		SpringApplication.run(AdministrativoApplication.class, args);
 	}
 
 }
